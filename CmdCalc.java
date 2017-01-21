8853c98a455792d6112c0ee0dd8998bb8ad7de27
Ibrahim-Abdullah <braimahabdulla@gmail.com>
2017-01-20 17:22:40 -0800

    Addition functionality completed

---
+++ CmdCalc.java
@@ -0,0 +1,16 @@
+public class CmdCalc{
+
+	public static void main (String args[]){
+		String function  = args[0];
+
+		double first_operand = Double.parseDouble(args[1]);
+		double second_operand = Double.parseDouble(args[2]);
+
+		switch(function){
+			case "Add":
+			case "add":
+			case "addition":
+				System.out.println(first_operand+second_operand);
+		}
+	}
+}
